3c36b4addf50d52a4fb4521380eb99712da0ee4a
willpine <willianyuti@gmail.com>
2021-08-29 23:26:21 -0300

    Adição de comentários

--- FirestoreClient.js
+++ FirestoreClient.js
@@ -4,7 +4,8 @@ const path = require('path');
 // We setup and export an instance of this class
 // so we know only one instance is necessary
 class FirestoreClient {
-    // We setup auth inside the constructor
+    // We setup auth inside the constructor, so everytime we call firestore, it is authenticated.
+    // You have to get the .json from the service account on gcp, and then set the keyFilename
     constructor() {
         this.firestore = new Firestore({
             projectId: 'sapient-rune-324212',
--- Index.js
+++ Index.js
@@ -1,5 +1,7 @@
+//Importando classe com functions para CRUD
 const FirestoreClient = require('./FirestoreClient');
 
+// Criando objetos para popular o banco
 const cc8p68 = {
     docName: 'CC8P68',
     campus: 'Paraíso'
@@ -17,16 +19,22 @@ const alunoCleitinho = {
     status: 'Active'
 }
 
+// Caminho que aponta para o recurso desejado
 const path = 'turmas/CC8P68/Alunos/Marcos'
 
+// Para testar, mudo o caminho conforme o objeto que desejo
+
+// Cria um documento cc8p68 na collection turmas
 const save = async () => {
     await FirestoreClient.saveInCollection('turmas', cc8p68);
 }
 
+// Cria uma subcollection dentro do doc cc8p68 chamada Alunos, e nela salva o doc alunoWill
 const saveInCollection = async () => {
     await FirestoreClient.saveInSubCollection('turmas',cc8p68.docName,'Alunos',alunoWill);
 }
 
+// Salva o json alunoCleitinho como um documento no caminho especificado
 const saveByPath = async () => {
     await FirestoreClient.saveByPath(path, alunoCleitinho);
 }
@@ -43,4 +51,5 @@ const deleteByPath = async () => {
     await FirestoreClient.delete(path);
 }
 
+// As funções anteriores estão apenas declaradas, para rodá-las, chame-as aqui
 readByPath();
